090fe828f002704c7bfe737bba5bc09a794d2b26
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2018-12-23 00:55:32 +0900

    GCD

--- caddi/gcd.cpp
+++ caddi/gcd.cpp
@@ -2,54 +2,69 @@
 #include<math.h>
 using namespace std;
 
-long euclid(long a, long b) {
-    if (a<b) {
-        long tmp = a;
-        a = b;
-        b = tmp;
-    }
-    if (b==0) {
-        return a;
-    }
-    long r = a % b;
-    while (r!=0) {
-        a = b;
-        b = r;
-        r = a % b;
-    } 
-  return b;
-}
+long N, P;
 
-long devide(long N, int nn, long P) {
-    long m = ceil(sqrt(P));
-    long gcd=0, p=P, q=1;
-    for (long i=m; i>0; i--){
-        if (P%i==0) {
-            long j = P/i;
-            long tmp = euclid(j, i);
-            if (tmp>gcd) {
-                gcd = tmp;
-                p = j;
-                q = i;
+void eratos(long primes[], long size) {
+    for (long i=1; i<size; i++) {
+        if (primes[i]==1) {
+            long t = 1;
+            while (i+t*(i+1)<size) {
+                primes[i+t*(i+1)] = 0;
+                t += 1;
             }
         }
-    }
-    cout << nn << " " << gcd << " " << p << " " << q << endl;
-    if (pow(2, nn)>=N) {
-        if (gcd==1) {
-            return 0;
+    } 
+}
+
+void prime_factorization(long fact[], long primes[], long size) {
+    long d = P;
+    for (long i=1; i<size; i++) {
+        if (primes[i]==1) {
+            long tmp = pow((i+1),N);
+            while (d%tmp ==0) {
+                fact[i] += 1;
+                d /= tmp;
+            }
         }
-        return gcd;
+        if (d==0) {
+            return;
+        }
     }
-    long g1 = devide(N, nn+1, p);
-    long g2 = devide(N, nn+1, q);
-    return ;
+}
+
+long mul_vec(long fact[], long size) {
+    int gcd = 1;
+    for (long i=1; i<size; i++) {
+        if (fact[i]>0) {
+            gcd *= pow(i+1, fact[i]);
+        }
+    }
+    return gcd;
 }
 
 int main() {
-    long N, P;
     cin >> N >> P;
-    devide(N, 1, P);
-    //cout << devide(n) << endl;
+    if (N==1) {
+        cout << P << endl;
+        return 0;
+    }
+
+    long n = ceil(pow(P, 1.0/N));
+
+    long primes[n]; // Binary for the prime indices
+    primes[0] = 0;
+    for (long i=1; i<n; i++) {
+        primes[i] = 1;
+    }
+    eratos(primes, n);
+
+    long fact[n]; // Prime factorization of P^(1/N)
+    for (long i=0; i<n; i++) {
+        fact[i] = 0;
+    }
+    prime_factorization(fact, primes, n);
+
+    long gcd = mul_vec(fact, n);
+    cout << gcd << endl;
     return 0;
 }

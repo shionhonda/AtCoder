4eeee340c4f13bfd6e1aa3643212556cd62960bd
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2019-05-25 19:48:46 +0900

    Even Relation

--- 126/even.py
+++ 126/even.py
@@ -1,49 +1,30 @@
 N = int(input())
-arr = [None]*N
-visited = [False]*N
-UVW = []
 
-class Node:
-    def __init__(self, id):
-        self.id = id
-        self.friends = []
-        self.color = None
+q = []
+visited = [0] * N
+ans = [0] * N
+A = [[] for _ in range(N)]  # tree
+W = [[] for _ in range(N)]  # weight
 
-    def add_friend(self, friend, w):
-        self.friends.append([friend.id, w)
-        friend.friends.append([self.id, w])
-        
-nodes = [Node(i) for i in range(1,N+1)]
 for i in range(N-1):
-    u, v, w = map(int, input().split())
-    nodes[u-1].add_friend(nodes[v-1])
-    
-def color_friend(node):
-    for friend,w in node.friends:
-        if friend.color is not None:
-            continue
-        
+    u,v,w = map(int, input().split())
+    u -= 1
+    v -= 1
+    A[u].append(v)
+    A[v].append(u)
+    W[u].append(w)
+    W[v].append(w)
 
+q.append(0)  # start
+for _ in range(N):
+    u = q.pop(0)
+    visited[u] = 1
+    k = 0
+    for v in A[u]:
+        if visited[v]==0:
+            q.append(v)
+            ans[v] = ans[u] + W[u][k]
+        k += 1
 
-while True:
-
-
-
-for i,uvw in enumerate(UVW):
-    u,v,w = uvw
-    if i==0:
-        arr[u-1] = 0
-        visited[u-1] = True
-    if visited[u-1]:
-        arr[v-1] = arr[u-1] + w
-        visited[v-1] = True
-    elif visited[v-1]:
-        arr[u-1] = arr[v-1] + w
-        visited[u-1] = True
-    
-print(arr)
-for a in arr:
-    if a%2==0:
-        print(0)
-    else:
-        print(1)
+for a in ans:
+    print(a%2)

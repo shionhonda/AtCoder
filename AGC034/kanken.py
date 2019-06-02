d5f288730d70511a1406369bb3009a3571cb5fbd
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2019-06-02 22:37:34 +0900

    AB

--- AGC034/kanken.py
+++ AGC034/kanken.py
@@ -29,31 +29,20 @@ def cd():
     return 'Yes'
 
 def dc():
-    block_cnt = 0
-    space_cnt = 0
-    if S[D-1]=='.' and S[D+1]=='.':
-        space_flg = True
-    else: 
-        space_flg = False
-    for i,s in enumerate(S[A:C+1]):
-        if s=='#':
-            block_cnt += 1
-            if B<=i+A and i+A<D and (not space_flg):
-                space_cnt = 0
+    two_block_flg = False
+    for s in S[A:C+1]:
+        if two_block_flg:
+            if s=='#':
+                return 'No'
+            else:
+                two_block_flg = False
         else:
-            block_cnt = 0
-            if B<=i+A and i+A<D and (not space_flg):
-                space_cnt += 1
-        #print(i+A, block_cnt, space_cnt)
-        if block_cnt>=2:
-            return 'No'
-        if space_cnt>=3:
-            space_flg = True
-    if space_flg:
-        return 'Yes'
-    else:
-        return 'No'
-                
+            if s=='#':
+                two_block_flg = True
+    for i in range(B, D+1):
+        if S[i-1]=='.' and S[i]=='.' and S[i+1]=='.':
+            return 'Yes'
+    return 'No'  
 
 
 if C<D:

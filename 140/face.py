b25d06e0b2c42818b02d4e83fdb9340ef3291e25
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2019-09-08 11:29:44 +0900

    D

--- 140/face.py
+++ 140/face.py
@@ -1,3 +1,12 @@
 N, K = map(int, input().split())
 S = input()
 
+ans = 0
+for i in range(N-1):
+    if S[i] == S[i+1]:
+        ans += 1
+
+if ((N-1)-ans) < 2*K:
+    print(N-1)
+else:
+    print(ans + 2*K)
